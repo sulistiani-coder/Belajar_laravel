4b76eccd7c092855407f1c852ddc61844ac9b537
N4syaa <sulistianilombok2021@gmail.com>
2026-04-21 08:54:15 +0700

    Remove unused Request import from HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class HomeController extends Controller
 {
     public function index()
@@ -11,3 +9,6 @@ class HomeController extends Controller
         return view('home');
     }
 }
+
+
+
