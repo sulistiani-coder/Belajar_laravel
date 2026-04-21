777025946891789c9102b6ffb8c8f8d87888fb48
N4syaa <sulistianilombok2021@gmail.com>
2026-04-21 09:51:38 +0700

    Fix syntax errors in SearchController: replace double single quotes with proper double quotes

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -10,71 +10,71 @@ class SearchController extends Controller
 {
     public function index()
     {
-        return view(''search.index'');
+        return view("search.index");
     }
 
     public function results(Request $request)
     {
-        $query = $request->query(''q'');
+        $query = $request->query("q");
         
         if (!$query) {
-            return view(''search.results'', [
-                ''query'' => '''',
-                ''posts'' => collect(),
-                ''products'' => collect(),
-                ''total'' => 0
+            return view("search.results", [
+                "query" => "",
+                "posts" => collect(),
+                "products" => collect(),
+                "total" => 0
             ]);
         }
 
         // Search in Posts
-        $posts = Post::where(''title'', ''LIKE'', "%{$query}%")
-                    ->orWhere(''content'', ''LIKE'', "%{$query}%")
+        $posts = Post::where("title", "LIKE", "%{$query}%")
+                    ->orWhere("content", "LIKE", "%{$query}%")
                     ->get();
 
         // Search in Products
-        $products = Product::where(''name'', ''LIKE'', "%{$query}%")
-                          ->orWhere(''description'', ''LIKE'', "%{$query}%")
+        $products = Product::where("name", "LIKE", "%{$query}%")
+                          ->orWhere("description", "LIKE", "%{$query}%")
                           ->get();
 
         $total = $posts->count() + $products->count();
 
-        return view(''search.results'', [
-            ''query'' => $query,
-            ''posts'' => $posts,
-            ''products'' => $products,
-            ''total'' => $total
+        return view("search.results", [
+            "query" => $query,
+            "posts" => $posts,
+            "products" => $products,
+            "total" => $total
         ]);
     }
 
     public function advanced()
     {
-        return view(''search.advanced'');
+        return view("search.advanced");
     }
 
     public function advancedResults(Request $request)
     {
         // Implement advanced search results
-        return view(''search.advanced_results'');
+        return view("search.advanced_results");
     }
 
     public function suggestions(Request $request)
     {
         // Return JSON suggestions
-        return response()->json([''suggestions'' => []]);
+        return response()->json(["suggestions" => []]);
     }
 
     public function history()
     {
-        return view(''search.history'');
+        return view("search.history");
     }
 
     public function trending()
     {
-        return view(''search.trending'');
+        return view("search.trending");
     }
 
     public function filters()
     {
-        return view(''search.filters'');
+        return view("search.filters");
     }
 }
